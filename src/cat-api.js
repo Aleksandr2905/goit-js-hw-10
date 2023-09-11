9c1df8c8c7402dcf238c8d37a7662086b7620f7b
Aleksandr2905 <natalukha@ukr.net>
2023-09-11 22:36:00 +0300

    cat-api

---
+++ src/cat-api.js
@@ -0,0 +1,37 @@
+// import axios from "axios";
+// axios.defaults.headers.common["x-api-key"] = "live_k3OMSNQwJi1Dvsi9dzeUebFhUVuiAzo4v5ta3ytK9u4fqZxzpGK3tbU70NIATd5s";
+
+
+const refs = {
+    BASE_URL: 'https://api.thecatapi.com/v1/',
+    BREEDS: 'breeds',
+    IMG: '/images/search',
+    API_KEY: 'live_k3OMSNQwJi1Dvsi9dzeUebFhUVuiAzo4v5ta3ytK9u4fqZxzpGK3tbU70NIATd5s',
+};
+
+
+export function fetchBreeds() {
+    return fetch(`${refs.BASE_URL}${refs.BREEDS}?${refs.API_KEY}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+            
+        })
+       
+};
+
+
+export function fetchCatByBreed(breedId) {
+    return fetch(`${refs.BASE_URL}${refs.IMG}?${breedId}`)
+    .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+        })
+};
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
-import axios from "axios";
-axios.defaults.headers.common["x-api-key"] = "live_k3OMSNQwJi1Dvsi9dzeUebFhUVuiAzo4v5ta3ytK9u4fqZxzpGK3tbU70NIATd5s";
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
+
+const refs
