56054c96e2e367cf7dcc432e35e2439f99da7c05
Aleksandr2905 <natalukha@ukr.net>
2023-09-13 00:17:17 +0300

    final hw 10

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,38 +1,17 @@
-// import axios from "axios";
-// axios.defaults.headers.common["x-api-key"] = "live_k3OMSNQwJi1Dvsi9dzeUebFhUVuiAzo4v5ta3ytK9u4fqZxzpGK3tbU70NIATd5s";
-
-
-const refs = {
-    BASE_URL: 'https://api.thecatapi.com/v1/',
-    BREEDS: 'breeds',
-    IMG: '/images/search',
-    API_KEY: 'live_k3OMSNQwJi1Dvsi9dzeUebFhUVuiAzo4v5ta3ytK9u4fqZxzpGK3tbU70NIATd5s',
-};
+import axios from "axios";
+axios.defaults.headers.common["x-api-key"] = "live_k3OMSNQwJi1Dvsi9dzeUebFhUVuiAzo4v5ta3ytK9u4fqZxzpGK3tbU70NIATd5s";
 
 
 export function fetchBreeds() {
-    return fetch(`${refs.BASE_URL}${refs.BREEDS}?${refs.API_KEY}`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(response.data);
-            }
-            return response.json();
-            
-        })
-       
-};
+    return axios.get(`https://api.thecatapi.com/v1/breeds`)
+        
+    };
 
 
 export function fetchCatByBreed(breedId) {
-    return fetch(`${refs.BASE_URL}${refs.IMG}?${breedId}`)
-    .then(response => {
-            if (!response.ok) {
-                throw new Error(response.data);
-            }
-            return response.json();
-    })
+    return axios.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
     
-};
+    };
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,86 @@
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
+
 
 const refs = {
     breedSelectEl: document.querySelector('.breed-select'),
+    loadingEl: document.querySelector('.loading'),
     loaderEl: document.querySelector('.loader'),
     errorEl: document.querySelector('.error'),
     catInfoEl: document.querySelector('.cat-info'),
 };
+
+refs.breedSelectEl.addEventListener('change', OnBreedSelectElChange);
+
+fetchBreeds()
+    .then(response => {
+        refs.breedSelectEl.classList.remove('visually-hidden');
+        refs.loadingEl.classList.add('visually-hidden');
+        refs.loaderEl.classList.add('visually-hidden');
+        refs.breedSelectEl.innerHTML = createMarkup(response.data);
+        new SlimSelect({
+            select: refs.breedSelectEl,
+            settings: {
+            showSearch: false,
+            },
+        });
+    })
+    .catch(err => {
+        refs.loadingEl.classList.add('visually-hidden');
+        refs.loaderEl.classList.add('visually-hidden');
+      Notify.failure(`${refs.errorEl.textContent}`);
+    })
+
+    function createMarkup(arr) {
+        return arr.map(({ id, name }) =>
+            `<option value="${id}">${name}</option>`
+        ).join('');    
+
+     
+};
+
+
+function OnBreedSelectElChange(event) {
+    refs.loadingEl.classList.remove('visually-hidden');
+    refs.loaderEl.classList.remove('visually-hidden');
+    refs.errorEl.classList.add('visually-hidden');
+    refs.catInfoEl.classList.add('visually-hidden');
+    const breedElId = event.target.value;
+    
+
+    fetchCatByBreed(breedElId)
+        .then(response => {
+            refs.loadingEl.classList.add('visually-hidden');
+            refs.loaderEl.classList.add('visually-hidden');
+            refs.catInfoEl.classList.remove('visually-hidden');
+            refs.catInfoEl.innerHTML = createCatInfoMarkup(response.data[0])
+        })
+        .catch(err => {
+            refs.loadingEl.classList.add('visually-hidden');
+            refs.loaderEl.classList.add('visually-hidden');
+            Notify.failure(`${refs.errorEl.textContent}`);
+        });
+    
+    function createCatInfoMarkup(
+        { breeds: {
+         0: { temperament, name, description },
+        }, url }) {
+        return `
+        <img src="${url}" alt="${name}" width="600" />
+        <div class="wrapper">
+        <h2>${name}</h2>
+        <p>${description}</p>
+        <h3>Temperament:</h3>
+        <p>${temperament}</p>
+        </div>`        
+    };
+
+
+};
+
+
 
 
